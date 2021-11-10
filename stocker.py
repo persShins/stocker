a2179547f8d3bc72656bc7e98dd08554f3d69cf2
PersShin <shinryan9@naver.com>
2021-11-10 22:03:28 +0900

    *Basic 함수 수정(종목명이 긴 경우 예외 추가) *Kospi 전종목 함수 추가

--- stocker.py
+++ stocker.py
@@ -3,19 +3,24 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import plotly.graph_objects as go
+
 url = "https://finance.naver.com/sise/sise_market_sum.nhn?page=1"
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'lxml')
-def Basic():
+
+def Basic(url=None):
     stock_data=[]
-    url = "https://finance.naver.com/sise/sise_market_sum.nhn?page=1"
+    if url==None:
+        url = "https://finance.naver.com/sise/sise_market_sum.nhn?page=1"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'lxml')
     stock_list = soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("tr")
     for stock in stock_list:
-        if len(stock) > 1 :
-            stock_data.append(stock.get_text().split()[2])
+        if(len(stock)>1):
+            tmp=stock.get_text().split()
+            stock_data.append(tmp[len(tmp)-10])
     return stock_data
+    
 def Kospi50():
     code=[]
     stock_Url=soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("a", attrs={"class": "tltle"})
@@ -25,17 +30,21 @@ def Kospi50():
     dict={"Code":code, "Current_value":current_value}
     Data=pd.DataFrame(dict)
     return Data
-def Kospi500():#I've only thought about scrapping the first page of kospi
+
+def Kospi_All():
     code=[]
-    while True:
-        url = "https://finance.naver.com/sise/sise_market_sum.nhn?page="+36
+    current_value=[]
+    for i in range(1,36):
+        url = "https://finance.naver.com/sise/sise_market_sum.nhn?page="+str(i)
         print(url)
         res = requests.get(url)
         soup = BeautifulSoup(res.text, 'lxml')
+        #print(soup)
         stock_Url=soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("a", attrs={"class": "tltle"})
         for index, stock in enumerate(stock_Url):
             code.append(stock['href'].split('=')[1])
-    current_value=Basic()
+        current_value.extend(Basic(url))
+    print(current_value)
     dict={"Code":code, "Current_value":current_value}
-    Data=pd.DataFrame(dict)
-    return Data
+    data=pd.DataFrame(dict)
+    return data
